74ca75dbf65aa5c826a8489c6b5f907f6a05c582
MezhIrina <mezhenmi@gmail.com>
2026-02-09 16:34:55 +0500

    first commit

---
+++ readme.txt
@@ -0,0 +1,26 @@
+Смены флористов (ничего не выбрано) - florist-shifts.html
+Смены флористов (открыть смену) - florist-shifts-open.html
+Смены флористов (закрыть смену) - florist-shifts-close.html
+Смены флористов (модалки) - florist-shifts-modals.html
+
+Журнал инкассации (Инкасация) - florist-shifts-collection-journal-coll.html
+Журнал инкассации (Выдача из кассы) - florist-shifts-collection-journal-issuance.html
+Журнал пересчета - florist-shifts-conversion-journal.html
+
+Списать товары - write-list.html
+Создать списание и Взять в работу - write-create.html (тут есть элементы для продаж)
+
+Пустой чек - blank-check.html
+Заполненный чек - filling-check.html (есть скрытые элементы в панели управления, стили для всего прописаны)
+
+Справочники - directory.html
+Справочники(редактирование) - directory-edit.html
+
+Управление товарами (список товаров) - product-management.html
+Управление товарами (редактировать товар) - product-management-edit.html
+Управление товарами (новый товар) - product-management-new.html
+Управление товарами (новый товар, не заполненный) - product-management-new-clear.html
+Управление товарами (форма Пересчитать цену) - product-management-modal.html
+
+Состав товара (Список товаров) - product-composition.html
+Состав товара (Выбрать из витрины) - product-composition-showcase.html
---
+++ script/main.js
@@ -0,0 +1,93 @@
+//Кастомный селект
+function CustomSelect(options) {
+    var elem = options.elem;
+  
+    elem.onclick = function(event) {
+        if (event.target.className == 'title') {
+            toggle();
+        } else if (event.target.tagName == 'LI') {
+            setValue(event.target.innerHTML, event.target.dataset.value);
+            event.target.classList.add('active');
+            close();
+        }
+    }
+  
+    var isOpen = false;
+  
+    // ------ обработчики ------
+  
+    // закрыть селект, если клик вне его
+    function onDocumentClick(event) {
+        if (!elem.contains(event.target)) close();
+    }
+  
+    // ------------------------
+  
+    function setValue(title, value) {
+        let selectItem = elem.querySelectorAll('li.active');
+
+        for(let i = 0; i < selectItem.length; i++) {
+            // Убираем у других
+            selectItem[i].classList.remove('active');
+        }
+        
+        elem.querySelector('.title').innerHTML = title;
+        
+        var widgetEvent = new CustomEvent('select', {
+            bubbles: true,
+            detail: {
+                title: title,
+                value: value
+            }
+        });
+
+        elem.dispatchEvent(widgetEvent);
+    }
+  
+    function toggle() {
+        if (isOpen) close()
+        else open();
+    }
+  
+    function open() {
+        elem.classList.add('open');
+        document.addEventListener('click', onDocumentClick);
+        isOpen = true;
+    }
+  
+    function close() {
+        elem.classList.remove('open');
+        document.removeEventListener('click', onDocumentClick);
+        isOpen = false;
+    }
+}
+
+$('document').ready(function() {
+    let customSelects = $('*[data-customselect]');
+
+    customSelects.each(function(i) {
+        new CustomSelect({
+            elem: document.getElementById($(this).attr('id'))
+        });
+    });
+
+
+
+    //Инициализация скролла
+    if(document.querySelector('.tabs-viewport')) {
+        new ScrollBooster({
+            viewport: document.querySelector('.tabs-viewport'),
+            content: document.querySelector('.tabs-control'),
+            scrollMode: 'transform',
+            direction: 'horizontal',
+            emulateScroll: true,
+        });
+    }
+});
+
+//Кастомная кнопка добавления файла
+if(document.querySelector('.file-up')) {
+    document.querySelector('.file-up').onclick = function () {
+        document.getElementById(this.dataset.fUp).click();
+    };
+}
